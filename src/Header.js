550cfc61fa83f6d0fa8457f4346d87ebe7068f8a
fasial634 <faisalahmed634@gmail.com>
2024-03-31 09:46:39 +0800

    version 0.0.2

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
-import logo from './assests/logo2.jpg'; 
+
 
 
 function Header() {
     return (
         <div className="header card">
-           <img src={logo} alt="logoOne" />
+           {/* <img src={logo} alt="logoOne" /> restaurant logo   */}
+           <h4>restaurant logo</h4>
         </div>
     )
 }
