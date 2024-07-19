ad8b595040c8814445175ab819f21f86e2b7959e
fasial634 <69841975+fasial634@users.noreply.github.com>
2024-07-19 11:31:09 +0800

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 
 
 
+import logo from './assests/logo2.jpg'; 
+
+
 function Header() {
     return (
         <div className="header card">
-           {/* <img src={logo} alt="logoOne" /> restaurant logo   */}
-           <h4>restaurant logo</h4>
+           <img src={logo} alt="logoOne" />
         </div>
     )
 }
